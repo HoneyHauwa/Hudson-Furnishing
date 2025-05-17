6058e24b4c65b0e5e544118ed458cbb8bb763756
honey hauwa <hauwa1602@gmail.com>
2025-05-17 18:59:41 +0100

    Update colors in CSS for better visibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/Footer';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Menu from './pages/Menu';
 import About from './pages/About';
